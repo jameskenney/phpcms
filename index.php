66d5c9856abe3d9d993e0ef5f5894fe2b969facc
James Kenney <james@jjedmond.com>
2018-03-24 21:13:37 -0500

    Pulling FP blog content from DB

--- index.php
+++ index.php
@@ -3,14 +3,33 @@
 require_once( __DIR__ . "/admin/head.php" );
 ?>
    <main id="content" class="main-content">
-      <h1>Determination of motivation</h1>
 
-      <p>Motivation is a multi-level concept. Even the blinking of the brain requires motivation, not to mention the motivation to get involved in
-         semi-annual projects. As the actions are invested in each other, so are the investments and motivations. The motivation to talk with a
-         colleague is invested in the motivation to better understand the task that is invested in the motivation to qualitatively cope with the
-         functional for which you are responsible, which in turn is part of the motivation in time and in due quality to release the product. Lower
-         levels of motivation are necessary for the existence of the upper levels, but the upper levels also support the lower ones. For example, if
-         you love the product, this gives you an extra reason to take care of the quality of the task, even if you do not like it.</p>
+       <!-- Grab front page content from DB and display it  -->
+	   <?php
+	   $query = "SELECT * FROM posts";
+	   $select_all_posts_query = mysqli_query($connection,$query);
+
+	   while($row = mysqli_fetch_assoc($select_all_posts_query)){
+		   $post_title = $row['post_title'];
+		   $post_author = $row['post_author'];
+		   $post_date = $row['post_date'];
+		   $post_content = $row['post_content'];
+
+		   ?>
+           <h1>
+               <a href="#"><?php echo $post_title; ?></a>
+           </h1>
+           <p>
+               Written on <?php echo $post_date; ?> by <?php echo $post_author; ?>
+           </p>
+           <p><?php echo $post_content; ?></p>
+           <a class=""btn btn-primary" href="#">Read More<span class="glyphicon glyphicon-chevron-right"></span></a>
+
+
+
+
+
+	   <?php } ?>
 
    </main> <!-- main -->
    <aside class="sidebar">
