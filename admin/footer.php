a6e1016bc9a06c0c882ef2d916f0508b927adfb5
Danny G Smith <dgs@riskiii.com>
2018-03-30 18:58:16 -0500

    makeing changes for chosen

--- admin/footer.php
+++ admin/footer.php
@@ -1,7 +1,6 @@
-   <footer class="colophon grid">
-      footer
-   </footer>
-</div> <!-- .wrapper -->
+<footer class="colophon grid">
+   footer
+</footer></div> <!-- .wrapper -->
 
 <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
         crossorigin="anonymous"></script>
