e84af79952d226e5eaf5a5fcdce3a758e6a3e6cb
Danny G Smith <dgs@riskiii.com>
2018-04-02 21:18:11 -0500

    added simple menu to top

--- sidebar.php
+++ sidebar.php
@@ -6,10 +6,6 @@
  * Time: 9:34 PM
  */
 ?>
-
-
-
-
 <aside class="sidebar">
     <h3>The Sidebar</h3>
     <p>The sidebar typically contains things like a menu:</p>
@@ -29,17 +25,15 @@
         </div>
         </form> <!-- /Search Form -->
     </div>
-
-
     <ul>
-        <li><a href="login.php">login</a></li>
-        <li><a href="admin/exit.php">logout</a></li>
-        <li><a href="admin/register.php">register user</a></li>
-        <li><a href="admin/user-rolls.php">add/remove user roles</a></li>
-        <li><a href="admin/delete-users.php">delete user</a></li>
-        <li><a href="admin/change-password.php">change password</a></li>
-        <li><a href="admin/change-email.php">change email</a></li>
-        <li><a href="admin/login-as.php">login as</a></li>
-        <li><a href="admin/"></a></li>
+       <li><a href="/">home</a></li>
+       <li><a href="/login.php">login</a></li>
+        <li><a href="/admin/exit.php">logout</a></li>
+        <li><a href="/admin/register.php">register user</a></li>
+        <li><a href="/admin/user-roles.php">add/remove user roles</a></li>
+        <li><a href="/admin/delete-users.php">delete user</a></li>
+        <li><a href="/admin/change-password.php">change password</a></li>
+        <li><a href="/admin/change-email.php">change email</a></li>
+        <li><a href="/admin/login-as.php">login as</a></li>
     </ul>
 </aside>
