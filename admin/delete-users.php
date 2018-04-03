3b4431fd2e2f6bbfca65c81f5cc98efa680b1f0d
Danny G Smith <dgs@riskiii.com>
2018-04-02 21:34:14 -0500

    add simple menu

--- admin/delete-users.php
+++ admin/delete-users.php
@@ -10,37 +10,78 @@ if ( isset( $_POST[ 'btn-login' ] ) ) {
 }
 ?>
 
-	<div class="user_listing">
 
-		<?php
-		try {
-			$user_list = $db->prepare(
-				"SELECT username, email FROM users ORDER BY id" );
-			$user_list->execute();
-		} catch ( Exception $e ) {
-			echo $e->getMessage();
-			die();
-		}
-		$user_display = $user_list->fetchAll( PDO::FETCH_ASSOC );
+<form class="signup-form-container" role="form" method="post"
+      id="login-form" name="login-form">
 
-		d($user_display);
-		?>
+    <div class="form-header">
+        <h3><span class="login-title"> Delete User</span>
+            <span class="login-title-right"><img src="../assets/fonts/solid/pencil-alt.svg" class="pencil-alt"></span>
+        </h3>
 
-		<div class="form-group">
-			<label for="sel1">User list:</label>
-			<select class="form-control" id="sel1">
-				<!-- option 1 is a baseline test-->
-				<option>1</option>
-				<option><?php
-					for ($user_display = 0; $user_display < count($user_list); $user_display++) {
-						print_r($user_display);
-					}
-					?></option>
-			</select>
-		</div>
+        <div id="error">
+			<?php
+			if ( isset( $error ) ) {
+				?>
+                <div class="alert alert-danger"> exclamation-triangle
+                    <img src="../assets/fonts/solid/exclamation-triangle.svg" class="user"> &nbsp; <?php echo $error; ?> !
+                </div>
+				<?php
+			}
+			?>
+        </div>
+    </div>  <!-- .form-header -->
 
-	</div>
+    <div class="form-body">
 
-<?php
-require_once( __DIR__ . "/footer.php" );
-?>
+        <!-- json response will be here -->
+        <div id="errorDiv"></div>
+        <!-- json response will be here -->
+
+        <div class="form-group">
+            <label for="sel1">Select User</label>
+            <select class="form-control" id="sel1">
+
+                <!-- Needs to pull users from the database and list-->
+
+                <option>User 1</option>
+                <option>User 2</option>
+                <option>User 3</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <div class="input-group">
+                <div class="input-group-addon"><img src="../assets/fonts/solid/lock.svg" class="clock"></span></div>
+                <input name="password" id="password" type="password" class="form-control" placeholder="Admin Password">
+            </div>
+            <span class="help-block" id="error"></span>
+        </div>
+
+        <div class="form-group">
+            <div class="input-group">
+                <div class="input-group-addon"><img src="../assets/fonts/solid/lock.svg" class="clock"></div>
+                <input name="cpassword" id="cpassword" type="password" class="form-control" placeholder="Retype Password">
+            </div>
+            <span class="help-block" id="error"></span>
+        </div>
+
+        <div class='clear_both'></div>
+        <div id='pass_meter'></div>
+
+
+    </div>
+    <div class="form-footer">
+        <div class="row">
+            <div class="form-group  left-col col-lg-6">
+                <button type="submit" name="btn-register" class="btn btn-info" id="btn-signup">
+                    <img src="../assets/fonts/solid/sign-in.svg" class="sign-in"> &nbsp; Delete Selected User
+                </button>
+            </div>
+
+            <!--            <div class="form-group  right-col col-lg-6">-->
+            <!--               <a href="forgot_password.php">Forgot password?</a>-->
+            <!--            </div>-->
+        </div>
+    </div>  <!-- .form-footer -->
+</form>
--- admin/login-as.php
+++ admin/login-as.php
@@ -7,8 +7,98 @@ if ( isset( $_POST[ 'btn-login' ] ) ) {
 	$umail = strip_tags( trim( $_POST[ 'uname_email' ] ) );
 	$upass = strip_tags( trim( $_POST[ 'password' ] ) );
 }
+?>
+
+<form class="signup-form-container" role="form" method="post"
+      id="login-form" name="login-form">
+
+	<div class="form-header">
+		<h3><span class="login-title">Login As</span>
+			<span class="login-title-right"><img src="../assets/fonts/solid/pencil-alt.svg" class="pencil-alt"></span>
+		</h3>
+
+		<div id="error">
+			<?php
+			if ( isset( $error ) ) {
+				?>
+				<div class="alert alert-danger"> exclamation-triangle
+					<img src="../assets/fonts/solid/exclamation-triangle.svg" class="user"> &nbsp; <?php echo $error; ?> !
+				</div>
+				<?php
+			}
+			?>
+		</div>
+	</div>  <!-- .form-header -->
+
+	<div class="form-body">
+
+		<!-- json response will be here -->
+		<div id="errorDiv"></div>
+		<!-- json response will be here -->
+
+		<div class="form-group">
+			<div class="input-group input-group-relative">
+				<div class="input-group-addon"><img src="../assets/fonts/solid/user.svg" class="user"></div>
+				<input name="uname_uid" type="text" id="uid" class="form-control" placeholder="User ID or Name" maxlength="40"
+				       autofocus="true">
+			</div>
+			<span class="help-block" id="check-e"></span>
+		</div>
+
+		<div class="form-group">
+			<div class="input-group input-group-relative">
+				<div class="input-group-addon"><img src="../assets/fonts/solid/envelope.svg" class="user"></div>
+				<input name="uname_email" type="text" id="name" class="form-control" placeholder="Email Address" maxlength="40"
+				       autofocus="true">
+			</div>
+			<span class="help-block" id="check-e"></span>
+		</div>
 
 
 
+		<div class="form-group">
+			<div class="input-group">
+				<div class="input-group-addon"><img src="../assets/fonts/solid/lock.svg" class="clock"></span></div>
+				<input name="password" id="password" type="password" class="form-control" placeholder="Password">
+			</div>
+			<span class="help-block" id="error"></span>
+		</div>
+
+		<div class="form-group">
+			<div class="input-group">
+				<div class="input-group-addon"><img src="../assets/fonts/solid/lock.svg" class="clock"></div>
+				<input name="cpassword" id="cpassword" type="password" class="form-control"
+				       placeholder="Confirm Password">
+			</div>
+			<span class="help-block" id="error"></span>
+		</div>
+
+		<div class='clear_both'></div>
+		<div id='pass_meter'></div>
+
+
+	</div>
+	<div class="form-footer">
+		<div class="row">
+			<div class="form-group  left-col col-lg-6">
+				<button type="submit" name="btn-register" class="btn btn-info" id="btn-signup">
+					<img src="../assets/fonts/solid/sign-in.svg" class="sign-in"> &nbsp; Login
+				</button>
+			</div>
+
+			<!--            <div class="form-group  right-col col-lg-6">-->
+			<!--               <a href="forgot_password.php">Forgot password?</a>-->
+			<!--            </div>-->
+		</div>
+	</div>  <!-- .form-footer -->
+</form>
+
+
+
+
+
+
+<?php
 require_once( __DIR__ . "/footer.php" );
 
+?>
