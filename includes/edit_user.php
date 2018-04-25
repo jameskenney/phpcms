64f9d5cb280e62f113d312e7800bf9e2f5d61d0a
James Kenney <james@jjedmond.com>
2018-04-24 21:59:59 -0500

    Can edit username, last, first, role and password via edit_user.php

--- includes/edit_user.php
+++ includes/edit_user.php
@@ -12,18 +12,21 @@
 require_once( __DIR__ . '/../bootstrap.php' );
 require( '../admin/config.php' );
 include( '../admin/head_admin.php' );
+?>
 
 
 
+<?php  // Get request user id and database data extraction
 
-if(isset($_GET['edit_user'])) {
+if(isset($_GET['edit_user'])){
 
-	$the_user_id = $_GET['edit_user'];
+	$the_user_id =  $_GET['edit_user'];
 
-	$query        = "SELECT * FROM users_new WHERE user_id = $the_user_id ";
-	$select_users_query = mysqli_query( $connection, $query );
+	$query = "SELECT * FROM users_new WHERE user_id = $the_user_id ";
+	$select_users_query = mysqli_query($connection,$query);
 
-	while ( $row = mysqli_fetch_assoc( $select_users_query ) ) {
+	while($row = mysqli_fetch_assoc($select_users_query)) {
+
 		$user_id        = $row['user_id'];
 		$username       = $row['username'];
 		$user_password  = $row['user_password'];
@@ -32,29 +35,65 @@ if(isset($_GET['edit_user'])) {
 		$user_email     = $row['user_email'];
 		$user_image     = $row['user_image'];
 		$user_role      = $row['user_role'];
+
 	}
-}
+	?>
+
+	<?php  // Post request to update user
+
+	if(isset($_POST['edit_user'])) {
+
+		$user_firstname   = $_POST['user_firstname'];
+		$user_lastname    = $_POST['user_lastname'];
+		$user_role        = $_POST['user_role'];
+
+		// $post_image = $_FILES['image']['name'];
+		// $post_image_temp = $_FILES['image']['tmp_name'];
+
+		$username      = $_POST['username'];
+		$user_email    = $_POST['user_email'];
+		$user_password = $_POST['user_password'];
+		$post_date     = date('d-m-y');
+
+		if(!empty($user_password)) {
+
+			$query_password = "SELECT user_password FROM users_new WHERE user_id =  $the_user_id";
+			$get_user_query = mysqli_query($connection, $query_password);
+			confirmQuery($get_user_query);
+
+			$row = mysqli_fetch_array($get_user_query);
+
+			$db_user_password = $row['user_password'];
+
+			if($db_user_password != $user_password) {
+
+				$hashed_password = password_hash($user_password, PASSWORD_BCRYPT, array('cost' => 12));
+
+			}
+
+			$query = "UPDATE users_new SET ";
+			$query .="user_firstname  = '{$user_firstname}', ";
+			$query .="user_lastname = '{$user_lastname}', ";
+			$query .="user_role   =  '{$user_role}', ";
+			$query .="username = '{$username}', ";
+			$query .="user_email = '{$user_email}', ";
+			$query .="user_password   = '{$user_password}' ";
+			$query .= "WHERE user_id = {$the_user_id} ";
 
 
+			$edit_user_query = mysqli_query($connection,$query);
+
+			confirmQuery($edit_user_query);
 
 
-if(isset($_POST['edit_user'])) {
+			echo "User Updated" . " <a href='../admin/users.php'>View Users?</a>";
 
-	$user_firstname    = $_POST['user_firstname'];
-	$user_lastname     = $_POST['user_lastname'];
-	$user_role         = $_POST['user_role'];
-	$username          = $_POST['username'];
-	$user_email        = $_POST['user_email'];
-	$user_password     = $_POST['user_password'];
+		}
 
-	$user_password = password_hash($user_password, PASSWORD_BCRYPT, array('cost' => 10));
-	$query = "INSERT INTO users_new(user_firstname, user_lastname, user_role,username,user_email,user_password) ";
-	$query .= "VALUES('{$user_firstname}','{$user_lastname}','{$user_role}','{$username}','{$user_email}', '{$user_password}') ";
-	$edit_user_query = mysqli_query($connection, $query);
+	}
 
-	confirmQuery($edit_user_query);
-
-	echo "User Created: " . " " . "<a href='../admin/users.php'>View Users</a> ";
+} else {  // Redirects to home page is the user id is blank
+	header("Location: index.php");
 }
 ?>
 
@@ -62,35 +101,41 @@ if(isset($_POST['edit_user'])) {
 
     <div class="form-group">
         <label for="title">Firstname</label>
-        <input type="text" value="<?php echo $user_firstname ?>" class="form-control" name="user_firstname">
+        <input type="text" value="<?php echo $user_firstname; ?>" class="form-control" name="user_firstname">
     </div>
 
     <div class="form-group">
         <label for="post_status">Lastname</label>
-        <input type="text" value="<?php echo $user_lastname ?>" class="form-control" name="user_lastname">
+        <input type="text" value="<?php echo $user_lastname; ?>" class="form-control" name="user_lastname">
     </div>
 
     <div class="form-group">
         <select name="user_role" id="">
-            <option value="subscriber">Select Options</option>
-            <option value="admin">Admin</option>
-            <option value="subscriber">Subscriber</option>
+            <option value="subscriber"><?php echo $user_role; ?></option>
+                <?php
+                // If the user is an admin it will display subscriber as an option to change to, not both, and vice versa.
+                if($user_role == 'admin') {
+                    echo "<option value='subscriber'>subscriber</option>";
+                } else {
+                    echo "<option value='admin'>admin</option>";
+                }
+                ?>
         </select>
     </div>
 
     <div class="form-group">
         <label for="post_tags">Username</label>
-        <input type="text" value="<?php echo $username ?>" class="form-control" name="username">
+        <input type="text" value="<?php echo $username; ?>" class="form-control" name="username">
     </div>
 
     <div class="form-group">
         <label for="post_content">Email</label>
-        <input type="email" value="<?php echo $user_email ?>" class="form-control" name="user_email">
+        <input type="email" value="<?php echo $user_email; ?>" class="form-control" name="user_email">
     </div>
 
     <div class="form-group">
         <label for="post_content">Password</label>
-        <input type="password" value="<?php echo $user_password ?>" class="form-control" name="user_password">
+        <input type="password" value="<?php echo $user_password; ?>" class="form-control" name="user_password">
     </div>
 
     <div class="form-group">
